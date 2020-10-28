837695d859ec5a15ec5fc33d4ee2a36e74efd5ce
SantiagoMartinez <santiago.martinez-m@mail.escuelaing.edu.co>
2020-10-28 11:42:38 -0500

    add guice for login bean

---
+++ src/main/java/edu/eci/cvds/guice/GuiceListener.java
@@ -0,0 +1,37 @@
+package edu.eci.cvds.guice;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import edu.eci.cvds.security.Logger;
+import edu.eci.cvds.security.LoggerApacheShiro;
+import edu.eci.cvds.view.*;
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class GuiceListener implements ServletContextListener {
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.removeAttribute(Injector.class.getName());
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Injector injector = Guice.createInjector(new XMLMyBatisModule() {
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.MySQL);
+                setEnvironmentId("development");
+                setClassPathResource("mybatis-config.xml");
+                bind(BasePageBean.class).to(LoginBean.class);
+                bind(Logger.class).to(LoggerApacheShiro.class);
+            }
+        });
+
+        servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
+    }
+
+    
+}
